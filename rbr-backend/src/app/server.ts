b678b2c0c21f72db3d3574cb281e25b4766ba805
= <=>
2024-05-18 01:08:33 -0300

    fix: ports

--- rbr-backend/src/app/server.ts
+++ rbr-backend/src/app/server.ts
@@ -2,6 +2,6 @@ import app from "./app"
 
 const PORT = process.env.PORT
 
-app.listen(PORT || 3333, () => {
+app.listen(PORT || 8000, () => {
     console.log(`Server is running on port ${PORT}`)
 })
